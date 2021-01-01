5d9c9e0d41cb5a4275f79dde80cca2be2e8b879d
gsoukup <gabor.soukup@gmail.com>
2021-01-01 21:24:02 +0100

    VBA and Folder choose

--- excelcollect.py
+++ excelcollect.py
@@ -2,5 +2,106 @@
 #-*-Coding:UTF8-*-
 #VBA remake in python for commuting EXCEL files
 
+import openpyxl
+
+path = ""
 
 
+"""
+
+Sub TSSARE()
+
+'
+'                                   Declaration
+'
+
+Dim w As ThisWorkbook
+
+Dim fajl As Variant
+Dim fajlok As Variant
+Dim id As Variant
+
+Dim path As String
+
+'
+'                                   Folder Select
+'
+
+With Application.FileDialog(msoFileDialogFolderPicker)
+    .Title = "Choose Folder!"
+
+If .Show = True Then
+path = .SelectedItems(1)
+
+
+Else
+path = ”nothing”
+
+End If
+End With
+
+'
+'                                   Datacopy
+'
+
+If path <> "nothing" Then
+
+'
+' i - Spacing for Macro Buttons, id-i = 1 becouse first id
+'
+
+i = 6
+id = 1
+'k = 0
+Application.DisplayAlerts = False
+
+fajl = Dir(path & "\*")
+
+Do While fajl <> "" 'Or k < 10
+
+Workbooks.Open (path & "\" & fajl)
+
+ThisWorkbook.Sheets(1).Cells(i + 1, 1).Value = id
+ThisWorkbook.Sheets(1).Cells(i + 1, 2).Value = Right(path, 8)
+ThisWorkbook.Sheets(1).Cells(i + 1, 3).Value = fajl
+
+'                                   Cover
+
+ActiveWorkbook.Sheets(1).Cells(8, 4).Copy Destination:=ThisWorkbook.Sheets(1).Cells(i + 1, 4)
+ActiveWorkbook.Sheets(1).Cells(9, 4).Copy Destination:=ThisWorkbook.Sheets(1).Cells(i + 1, 5)
+ActiveWorkbook.Sheets(1).Cells(10, 4).Copy Destination:=ThisWorkbook.Sheets(1).Cells(i + 1, 6)
+ActiveWorkbook.Sheets(1).Cells(11, 4).Copy Destination:=ThisWorkbook.Sheets(1).Cells(i + 1, 7)
+ActiveWorkbook.Sheets(1).Cells(12, 4).Copy Destination:=ThisWorkbook.Sheets(1).Cells(i + 1, 8)
+ActiveWorkbook.Sheets(1).Cells(13, 4).Copy Destination:=ThisWorkbook.Sheets(1).Cells(i + 1, 9)
+
+'                                   Clipboard empty
+Application.CutCopyMode = False
+
+
+
+ActiveWorkbook.Close (False)
+
+fajl = Dir()
+i = i + 1
+id = id + 1
+
+'k = k + 1
+
+
+
+Loop
+
+
+End If
+
+Application.DisplayAlerts = True
+
+End Sub
+"""
+from tkinter import filedialog
+from tkinter import *
+root = Tk()
+root.withdraw()
+path = filedialog.askdirectory()
+
+print(path)
